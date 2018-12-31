791721006cb3b60cdbcbe303b9ae36d0327b1305
Abhishek Prakash <prakashabhishek6262@gmail.com>
2018-12-31 22:54:51 +0530

    basic setup

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# deployed
-A deployer that helps to deploy the PHP Projects with ease.
+# deployer
+This application is responsible for handling the deployment of the PHP applications with ease.
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ *-----------------------------------------------------------------------
+ *  The Deployer
+ *-----------------------------------------------------------------------
+ *
+ * This application is responsible for handling the deployment of the
+ * PHP applications with ease.
+ * 
+ * @package: The Deployer
+ * @author: Abhishek Prakash <prakashabhishek6262@gmail.com>
+ * 
+ */
