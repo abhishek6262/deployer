ba16b1db9234b7113d3ba1298767955f9f7d4691
Abhishek Prakash <prakashabhishek6262@gmail.com>
2018-12-31 23:03:23 +0530

    change deployer introduction

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # deployer
-This application is responsible for handling the deployment of the PHP applications with ease.
+This acts as a Central Nervous System of the entire Deployment application.
+It does all the Hard work of deploying the application into the platform based
+on the environment and configurations provided or set by the user.
--- index.php
+++ index.php
@@ -5,10 +5,12 @@
  *  The Deployer
  *-----------------------------------------------------------------------
  *
- * This application is responsible for handling the deployment of the
- * PHP applications with ease.
- * 
+ * This acts as a Central Nervous System of the entire Deployment
+ * application. It does all the Hard work of deploying the application
+ * into the platform based on the environment and configurations
+ * provided or set by the user.
+ *
  * @package: The Deployer
  * @author: Abhishek Prakash <prakashabhishek6262@gmail.com>
- * 
+ *
  */
