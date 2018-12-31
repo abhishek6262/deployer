9f9070b52b609680ce3dac2e2a3a78cca6f9dc68
Abhishek Prakash <prakashabhishek6262@gmail.com>
2019-01-01 00:42:04 +0530

    add namespace to root.

--- .deployer/init.php
+++ .deployer/init.php
@@ -1,8 +1,11 @@
 <?php
 
+namespace App;
+
 /**
  * We'll begin by autoloading the application with Composer Protocols to
  * easily manage the loading of files and packages in the entire
  * ecosystem.
  */
+
 require __DIR__ . "/../vendor/autoload.php";
