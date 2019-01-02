f3b26e1a95a97883e339fddda955989a517bccc9
Abhishek Prakash <prakashabhishek6262@gmail.com>
2019-01-02 15:40:50 +0530

    simple files don't require namespacing.

--- app/init.php
+++ app/init.php
@@ -1,7 +1,5 @@
 <?php
 
-namespace App;
-
 /**
  * We'll begin by autoloading the files of the application with Composer
  * Protocols to initialize the classes automatically when required.
@@ -14,6 +12,6 @@ require __DIR__ . "/../vendor/autoload.php";
  * occuring in the application.
  */
 
-$client = new Adapters\SentryAdapter( new \Raven_Client( config( "SENTRY_DSN" ) ) );
-$errors = new ExceptionHandler( $client );
+$client = new \App\Adapters\SentryAdapter( new \Raven_Client( config( "SENTRY_DSN" ) ) );
+$errors = new \App\ExceptionHandler( $client );
 $errors->handle();
