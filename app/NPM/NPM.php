f06b03909830452ded192a6ab97c87a3a596a353
Abhishek Prakash <prakashabhishek6262@gmail.com>
2019-01-16 20:35:55 +0530

    okay

--- app/NPM/NPM.php
+++ app/NPM/NPM.php
@@ -43,7 +43,7 @@ class NPM
 
         shell_exec('
             export NVM_DIR=$HOME/nvm &&
-            curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34/install.sh | bash
+            curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34/install.sh | bash &&
             [ -s "$NVM_DIR/nvm.sh" ] &&
             \. "$NVM_DIR/nvm.sh" &&
             nvm install node &&
