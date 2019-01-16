cd69156a2aafe07f68dea99cebd2e7612328918f
Abhishek Prakash <prakashabhishek6262@gmail.com>
2019-01-16 20:51:06 +0530

    upgrade nvm version

--- app/NPM/NPM.php
+++ app/NPM/NPM.php
@@ -43,7 +43,7 @@ class NPM
 
         shell_exec('
             export NVM_DIR=$HOME/nvm &&
-            curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash &&
+            curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash &&
             [ -s "$NVM_DIR/nvm.sh" ] &&
             \. "$NVM_DIR/nvm.sh" &&
             nvm install node &&
