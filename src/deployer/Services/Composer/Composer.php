a01d484765bd8657ffde9ae1cabddac2d814b2a4
Abhishek Prakash <prakashabhishek6262@gmail.com>
2019-02-02 20:21:02 +0530

    Make composer instantiable
    Composer can manage multiple projects using a single binary file so it would be really handy to create different instances of composer for multiple projects rather than having to change directories again and again.

--- src/deployer/Services/Composer/Composer.php
+++ src/deployer/Services/Composer/Composer.php
@@ -1,137 +1,102 @@
 <?php
 
-namespace Deployer\Services\Composer;
-
 /**
  * Class Composer
- * @package Deployer\Services\Composer
  */
 class Composer
 {
     /**
      * @var string
      */
-    static $binPath = '/bin';
+    protected $binPath = '/bin';
 
     /**
      * @var string
      */
-    static $rootPath = '/';
+    protected $rootPath = '/';
 
     /**
-     * Determines whether Composer is installed in the environment.
+     * Composer constructor.
      *
-     * @return bool
+     * @param  string $rootPath
+     * @param  string $binPath
      */
-    public static function exists(): bool
+    public function __construct(string $rootPath, string $binPath)
     {
-        return file_exists(self::$binPath . "/composer") ? true : false;
+        $this->rootPath = $rootPath;
+        $this->binPath  = $binPath;
     }
 
     /**
-     * Performs an installation of Composer in the environment.
+     * Determines whether composer is installed in the project.
+     *
+     * @return bool
+     */
+    public function exists(): bool
+    {
+        return file_exists($this->binPath . "/composer") ? true : false;
+    }
+
+    /**
+     * Installs composer in the project.
      *
      * @return void
      */
-    public static function install(): void
+    public function install(): void
     {
         $CURRENT_WORKING_DIRECTORY = getcwd();
 
-        chdir(self::$rootPath);
+        chdir($this->rootPath);
 
         $MAX_EXECUTION_TIME = 1800; // "30 Mins" for slow internet connections.
 
         set_time_limit($MAX_EXECUTION_TIME);
 
         shell_exec("php -r \"copy('https://getcomposer.org/installer', 'composer-setup.php');\"");
-        shell_exec("php composer-setup.php --install-dir=" . self::$binPath . " --filename=composer");
+        shell_exec("php composer-setup.php --install-dir=" . $this->binPath . " --filename=composer");
         shell_exec("php -r \"unlink('composer-setup.php');\"");
 
         chdir($CURRENT_WORKING_DIRECTORY);
     }
 
     /**
-     * Performs an installation of Composer packages in the supplied
-     * directory.
-     *
-     * @param  string $directory
+     * Installs the packages present in the composer.json file.
      *
      * @return void
      */
-    public static function installPackages(string $directory = ''): void
+    public function installPackages(): void
     {
         $CURRENT_WORKING_DIRECTORY = getcwd();
 
-        if (empty($directory)) {
-            $directory = self::$rootPath;
-        }
-
-        chdir($directory);
+        chdir($this->rootPath);
 
         $MAX_EXECUTION_TIME = 1800; // "30 Mins" for slow internet connections.
 
         set_time_limit($MAX_EXECUTION_TIME);
 
-        shell_exec("php " . self::$binPath . "/composer install");
+        shell_exec("php " . $this->binPath . "/composer install");
 
         chdir($CURRENT_WORKING_DIRECTORY);
     }
 
     /**
-     * Determines whether the application requires the Composer packages
-     * to be installed.
-     *
-     * @param  string $directory
+     * Determines whether the project has packages to be installed.
      *
      * @return bool
      */
-    public static function packagesExists(string $directory = ''): bool
+    public function packagesExists(): bool
     {
-        if (empty($directory)) {
-            $directory = self::$rootPath;
-        }
-
-        return file_exists($directory . "/composer.json") ? true : false;
+        return file_exists($this->rootPath . "/composer.json") ? true : false;
     }
 
     /**
-     * Determines whether the Composer packages has been installed in the
-     * supplied directory.
-     *
-     * @param  string $directory
+     * Determines whether the packages are already installed in the
+     * project.
      *
      * @return bool
      */
-    public static function packagesInstalled(string $directory = ''): bool
+    public function packagesInstalled(): bool
     {
-        if (empty($directory)) {
-            $directory = self::$rootPath;
-        }
-
-        return file_exists($directory . "/vendor/") ? true : false;
-    }
-
-    /**
-     * Set the path on which the composer binary will be installed.
-     *
-     * @param string $path
-     *
-     * @return void
-     */
-    public static function setBinPath($path = ''): void
-    {
-        self::$binPath = realpath($path);
-    }
-
-    /**
-     * Set the path at which the composer commands will be executed.
-     *
-     * @param string $path
-     *
-     * @return void
-     */
-    public static function setRootPath($path = ''): void
-    {
-        self::$rootPath = realpath($path);
+        return file_exists($this->rootPath . "/vendor/") ? true : false;
     }
 }
