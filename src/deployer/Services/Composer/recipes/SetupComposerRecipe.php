0f710608ef89fe9d39f77ee9c7ea12c47d88661d
Abhishek Prakash <prakashabhishek6262@gmail.com>
2019-03-05 01:12:40 +0530

    Setup composer

--- src/deployer/Services/Composer/recipes/SetupComposerRecipe.php
+++ src/deployer/Services/Composer/recipes/SetupComposerRecipe.php
@@ -35,9 +35,35 @@ class SetupComposerRecipe extends Recipe
                 '/composer',
                 function (Container $container, Config $config, RecipeCollection $recipes, RouteCollection $routes)
                 {
+                    $composer = $container->resolve('composer');
+
+                    if ($composer->exists()) {
+                        $url = $routes->generate(
+                                    $routes->nextViewRoute()[3]
+                                );
+
+                        \redirect($url);
+                    }
+
+                    $url = $routes->generate('composer.install');
+
                     $response =
 <<<OUTPUT
-                        <h2 class="font-semibold" style="font-family: 'Open Sans';">Setup Composer</h2>
+                        <h2 class="font-semibold mb-10" style="font-family: 'Open Sans';">Setup Composer</h2>
+
+                        <p class="leading-loose mb-8" style="font-family: 'Lato';">
+                            Composer is needed in order to install PHP packages into the application.<br>
+                            We can automatically install this piece of software into your application and get it running instantly.<br>
+                            <span class="font-bold">Click on Install button to proceed.</span>
+                        </p>
+
+                        <form action="{$url}" method="post">
+                            <div class="text-right">
+                                <button type="submit" class="bg-indigo hover:bg-indigo-dark font-bold inline-block py-2 px-4 rounded text-white" style="font-family: 'Open Sans';">
+                                    Install
+                                </button>
+                            </div>
+                        </form>
 OUTPUT;
 
                     return new View($response);
@@ -48,8 +74,18 @@ OUTPUT;
             [
                 'post',
                 '/composer',
-                function () {
-                    // 
+                function (Container $container, Config $config, RecipeCollection $recipes, RouteCollection $routes) {
+                    $composer = $container->resolve('composer');
+
+                    if (! $composer->exists()) {
+                        $composer->install();
+                    }
+
+                    $url = $routes->generate(
+                                $routes->nextViewRoute()[3]
+                            );
+
+                    \redirect($url);
                 },
                 'composer.install'
             ]
