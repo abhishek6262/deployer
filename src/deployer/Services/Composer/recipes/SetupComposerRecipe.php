711b48e439d40bf248a5f91387be695088c9243b
Abhishek Prakash <prakashabhishek6262@gmail.com>
2019-03-05 16:38:29 +0530

    Update Composer Paragraph

--- src/deployer/Services/Composer/recipes/SetupComposerRecipe.php
+++ src/deployer/Services/Composer/recipes/SetupComposerRecipe.php
@@ -49,17 +49,17 @@ class SetupComposerRecipe extends Recipe
 
                     $response =
 <<<OUTPUT
-                        <h2 class="font-semibold mb-10" style="font-family: 'Open Sans';">Setup Composer</h2>
+                        <h2 class="font-semibold mb-10">Setup Composer</h2>
 
-                        <p class="leading-loose mb-8" style="font-family: 'Lato';">
-                            Composer is needed in order to install PHP packages into the application.<br>
-                            We can automatically install this piece of software into your application and get it running instantly.<br>
-                            <span class="font-bold">Click on Install button to proceed.</span>
+                        <p class="leading-loose mb-8">
+                            Composer is an application-level package manager for the PHP programming language that provides a standard format for managing dependencies of PHP software and required libraries. We can install Composer automatically into the project and get it running instantly.
                         </p>
+
+                        <p class="font-bold leading-loose mb-8">Click on Install button to proceed.</p>
 
                         <form action="{$url}" method="post">
                             <div class="text-right">
-                                <button type="submit" class="bg-indigo hover:bg-indigo-dark font-bold inline-block py-2 px-4 rounded text-white" style="font-family: 'Open Sans';">
+                                <button type="submit" class="bg-indigo hover:bg-indigo-dark font-bold inline-block py-2 px-4 rounded text-white">
                                     Install
                                 </button>
                             </div>
